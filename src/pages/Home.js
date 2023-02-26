3e8a2fabe7ac7d9aa602b0c3349bee0ed1f1a905
abirhasn07 <iamabirhasan07@gmail.com>
2023-02-26 07:18:52 -0800

    finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
 import { Box } from '@mui/material';
+import React, { useState } from 'react';
 
 import Exercises from '../components/Exercises';
-import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
